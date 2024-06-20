946b54f940cbf27d90698ac5415385fde364b9a3
Parth Katiyar <parthdev0101@gmail.com>
2024-06-20 21:29:38 +0530

    Configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './public/views/**/*.ejs', // Matches all .ejs files in views and subdirectories
+    './server/**/*.{html,js,jsx,ts,tsx}', // Matches all .html, .js, .jsx, .ts, and .tsx files in src and subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
