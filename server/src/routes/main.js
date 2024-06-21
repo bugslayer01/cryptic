708ae95bd5dab515e91e6989faabbaedff0a6fcb
Parth Katiyar <parthdev0101@gmail.com>
2024-06-21 18:01:17 +0530

    Added member registration

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -4,12 +4,18 @@ import Team from '../models/team.js';
 import User from '../models/user.js';
 const router = express.Router();
 
+const memberRegisterSchema = z.object({
+    teamName: z.string().min(1, { message: "Please enter the team name" }),
+    username: z.string().min(2, { message: "Username should be atleast 2 characters long" }),
+    email: z.string().email({ message: "Please enter a valid email address" }),
+    password: z.string().min(6, { message: "Password should be atleast 6 characters long" }),
+});
+
 router.get('/dashboard', checkAuth, async (req, res) => {
     try {
         if (!req.user) {
             res.redirect('/login')
         }
-        console.log(req.user)
         const user = await User.findById(req.user._id)
         const team = await Team.findById(user.teamId)
 
@@ -21,10 +27,11 @@ router.get('/dashboard', checkAuth, async (req, res) => {
 });
 
 router.get('/team', checkAuth, async (req, res) => {
+    if (!req.user) {
+        res.redirect('/login')
+    }
+
     try {
-        if (!req.user) {
-            res.redirect('/login')
-        }
         const user = await User.findById(req.user._id)
         const team = await Team.findById(user.teamId)
         const allIds = team.members;
@@ -40,6 +47,52 @@ router.get('/team', checkAuth, async (req, res) => {
     }
 });
 
-router.get
+router.get('/registermember', checkAuth, async (req, res) => {
+    if (!req.user) {
+        res.redirect('/login');
+    }
+    if (!req.user.isLeader) {
+        res.redirect('/team');
+    }
+    res.render('registerMember',{error : null})
+});
+
+router.post('/registermember', checkAuth, async (req, res) => {
+    if (!req.user) {
+        res.redirect('/login');
+    }
+    if (!req.user.isLeader) {
+        res.redirect('/team');
+    }
+
+    try {
+        const { username, email, password } = memberRegisterSchema.parse(req.body);
+        const leader = await User.findById(req.user._id);
+        const team = await Team.findById(leader.teamId);
+        const usernameExists = await User.findOne({ username })
+        const emailExists = await User.findOne({ email });
+        if (usernameExists) {
+            res.render('registerMember', { error: 'User with this username already registered' });
+        }
+        if (emailExists) {
+            res.render('registerMember', { error: 'Email already registered' });
+        }
+        const hash = await bcrypt.hash(password, 12);
+        const user = new User({
+            teamId: team._id,
+            username,
+            email,
+            password: hash,
+            isLeader: false,
+        });
+        let newUser = await user.save();
+        team.members.push(newUser._id)
+        await team.save();
+
+        res.redirect("/team");
+    } catch (error) {
+        res.render('registerMember', { error: error.errors[0].message });
+    }
+});
 
 export default router;
