7de61c380addf2ffda573f6a2ee6d1555df04b53
Parth Katiyar <parthdev0101@gmail.com>
2024-06-24 12:48:27 +0530

    Update mongoose.js

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Team from "../models/team.js";
+import User from '../models/user.js'
 dotenv.config();
 
 export default function connectMongo() {
     mongoose
         .connect(process.env.MONGODB_URI)
-        .then(() => {
+        .then(async () => {
             console.log("Connected to MongoDB");
+            // await User.deleteMany({}) For development, to be removed later
+            
+            // await Team.deleteMany({}) For development, to be removed later
         })
         .catch((err) => {
             console.log("Could not connect to MongoDB. Error:\n", err);
