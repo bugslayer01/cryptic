d11aa9632e2d31ded3824b53c330643a8f5860c3
Om Rajpal <omrajpal.exe@gmail.com>
2024-07-05 17:48:04 +0530

    designed the timer page

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -2,7 +2,6 @@ let countDownDate = new Date("Aug 7, 2024 14:00:00").getTime(); // will change t
 
 let timer = setInterval(() => {
     let now = new Date().getTime();
-
     let diff = countDownDate - now;
 
     let days = Math.floor(diff / (1000 * 60 * 60 * 24));
@@ -10,10 +9,11 @@ let timer = setInterval(() => {
     let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-    document.querySelector("#timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    let timerText = `${days}d:${hours}h:${minutes}m:${seconds}s`;
+    document.querySelector("#timer span").innerHTML = timerText;
     
     if (diff < 0) {
         clearInterval(timer);
-        document.querySelector("#timer").innerHTML = "Starting Soon";
+        document.querySelector("#timer span").innerHTML = "Starting Soon";
     }
 }, 1000);
