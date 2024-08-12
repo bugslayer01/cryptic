04d16969456d8de9c610d24d56f7a521c412109d
Parth Katiyar <parthdev0101@gmail.com>
2024-08-12 21:34:14 +0530

    Add try catch to eventActive middleware

--- server/src/middlewares/cryptic.js
+++ server/src/middlewares/cryptic.js
@@ -1,16 +1,25 @@
 import Control from '../models/settings.js'
 
 export default async function eventActive(req, res, next) {
-    if(!req.user) {
-        return res.redirect('/login');
+    try {
+        if (!req.user) {
+            return res.redirect('/login');
+        }
+
+        const control = await Control.findOne();
+
+        if (control.registrations) {
+            return res.redirect('/team');
+        }
+
+        if (!control.eventActive && !control.stats) {
+            return res.render('timer');
+        }
+
+        req.stats = control.stats;
+        return next();
+    } catch (error) {
+        console.error('Error in eventActive middleware:', error);
+        return res.status(500).send('Internal Server Error');
     }
-    const control = await Control.findOne();
-    if (control.registrations) {
-        return res.redirect('/team');
-    }
-    if (!control.eventActive && !control.stats) {
-        return res.render('timer');
-    }
-    req.stats = control.stats;
-    return next();
 }
